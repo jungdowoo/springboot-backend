64db68b9c20c55c835e8c0c296de3628aa52bf35
유용훈 <gkiioo2@gmail.com>
2024-10-05 23:49:07 +0900

    #DR-YYH-03
    @SpringbootTest 의 경우 DB가 정상적으로 연결된 테스트 환경이 갖추어야지만 사용 가능하기 때문에 임시 주석 처리 한다.

--- src/test/java/com/example/demo/JustartApplicationTests.java
+++ src/test/java/com/example/demo/JustartApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//@SpringBootTest
+public class JustartApplicationTests {
 
-@SpringBootTest
-class JustartApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+    //	@Test
+    void contextLoads() {
+    }
 
 }
